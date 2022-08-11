b44628ceb25759bd9d25af4e9deaf44e0497349a
Uday Kiran Irukula <77915031+udaykiran-i@users.noreply.github.com>
2022-08-11 21:50:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Section1 from "./components/section1/section1";
+// import Section2 from './components/section-2/section2';
+
+function App() {
+  return (
+    <div>
+      <Section1 />
+      {/* <Section2 /> */}
+    </div>
+  );
+}
+
+export default App;
